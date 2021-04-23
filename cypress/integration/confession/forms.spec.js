296c647b540e0c8a133621d3f875ebc4fb1f662e
Tosini <jbartos13@gmail.com>
2021-04-23 20:53:15 +0200

    fixed tests

--- cypress/integration/confession/forms.spec.js
+++ cypress/integration/confession/forms.spec.js
@@ -1,27 +1,22 @@
-describe('Service forms', () => {
-    beforeEach(() => {
-        cy.visit('localhost:3000/confessions');
-    })
+describe("Service forms", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000/confessions");
+  });
 
-    it('should to have button and open create service form', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(1)').click();
-        cy.get('.MuiDialog-paper form h2').contains('Create Confession');
-    })
+  it("should to have button and open create service form", () => {
+    cy.get("button").contains("Add").click({ force: true });
+    cy.get(".MuiDialog-paper form h2").contains("Create Confession");
+  });
 
+  it("should to open edit service form", () => {
+    cy.get("button").contains("Edit").click({ force: true });
+    cy.get('p[selectable="true"]').first().click();
+    cy.get(".MuiDialog-paper form h2").contains("Edit Confession");
+  });
 
-    it('should to open edit service form', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(2)').click();
-        cy.get('p[selectable="true"]').first().click();
-        cy.get('.MuiDialog-paper form h2').contains('Edit Confession');
-    })
-
-
-    it('should be able to delete paragraph', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(3)').click();
-        cy.get('p[selectable="true"]').first().click();
-        cy.get('.MuiDialog-paper h2').contains('Do you want to delete?');
-    })
-})
+  it("should be able to delete paragraph", () => {
+    cy.get("button").contains("Delete").click({ force: true });
+    cy.get('p[selectable="true"]').first().click();
+    cy.get(".MuiDialog-paper h2").contains("Do you want to delete?");
+  });
+});
--- cypress/integration/gallery/forms.spec.js
+++ cypress/integration/gallery/forms.spec.js
@@ -1,27 +1,22 @@
-describe('Album forms', () => {
-    beforeEach(() => {
-        cy.visit('localhost:3000/gallery')
-    })
+describe("Album forms", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000/gallery");
+  });
 
-    it('should to have button and open create Album form', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(1)').click();
-        cy.get('.MuiDialog-paper form h2').contains('Create Album');
-    })
+  it("should to have button and open create Album form", () => {
+    cy.get("button").contains("Add").click({ force: true });
+    cy.get(".MuiDialog-paper form h2").contains("Create Album");
+  });
 
+  it("should to open edit Album form", () => {
+    cy.get("button").contains("Edit").click({ force: true });
+    cy.get('img[action="true"]').first().click({ force: true });
+    cy.get(".MuiDialog-paper form h2").contains("Edit Album");
+  });
 
-    it('should to open edit Album form', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(2)').click();
-        cy.get('img[action="true"]').first().click({ force: true });
-        cy.get('.MuiDialog-paper form h2').contains('Edit Album');
-    })
-
-
-    it('should be able to delete Album', () => {
-        cy.get('button.MuiFab-root[tabindex="0"]').click();
-        cy.get('button.MuiFab-root[tabindex="-1"]:nth-child(3)').click();
-        cy.get('img[action="true"]').first().click({ force: true });
-        cy.get('.MuiDialog-paper h2').contains('Do you want to delete?');
-    })
-})
+  it("should be able to delete Album", () => {
+    cy.get("button").contains("Delete").click({ force: true });
+    cy.get('img[action="true"]').first().click({ force: true });
+    cy.get(".MuiDialog-paper h2").contains("Do you want to delete?");
+  });
+});
