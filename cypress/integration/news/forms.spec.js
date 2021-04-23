809aeda939c475b8ca5762410fdd0ea5fbd6d6f2
Tosini <jbartos13@gmail.com>
2021-04-23 20:57:40 +0200

    news tests fixed

--- cypress/integration/news/forms.spec.js
+++ cypress/integration/news/forms.spec.js
@@ -12,14 +12,14 @@ describe("News forms", () => {
   it("should to open edit news form", () => {
     cy.contains("News");
     cy.get("button").contains("Edit").click({ force: true });
-    cy.get('div[edition="true"]:nth-child(3)').click();
+    cy.get('div[edition="true"]').first().click({ force: true });
     cy.get(".MuiDialog-paper form h2").contains("Edit News");
   });
 
   it("should be able to delete news", () => {
     cy.contains("News");
     cy.get("button").contains("Delete").click({ force: true });
-    cy.get('div[edition="true"]:nth-child(3)').click();
+    cy.get('div[edition="true"]').first().click({ force: true });
     cy.get(".MuiDialog-paper h2").contains("Do you want to delete?");
   });
 });
